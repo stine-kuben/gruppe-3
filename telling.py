24a5e355e21609a79565af8b10987bb06ed8168f
averieyy <58344917+averieyy@users.noreply.github.com>
2025-11-12 09:42:28 +0100

    Added function to double every number in a list and a test for this function

--- telling.py
+++ telling.py
@@ -1,1 +1,4 @@
 
+
+def doble_liste(liste: list[float]):
+    return [tall * 2 for tall in liste]
--- test_telling.py
+++ test_telling.py
@@ -1,1 +1,5 @@
-test
+from telling import doble_liste
+
+
+def test_doble_liste():
+    assert doble_liste([1, 3, 4]) == [2, 6, 8]
